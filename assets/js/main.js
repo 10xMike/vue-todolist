e0e15b876dfaef8dde30a716e7e4747a99b2eb99
10xMike <michelegec@gmail.com>
2021-05-20 19:16:09 +0200

    Aggiunto trash container e metodo empty

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,47 +13,57 @@ Da tenere a mente:
 Ricordate di scrivere le istruzioni prima di iniziare a lavorare cercate di descrivere in modo sintetico gli step che pensate siano necessari. Consultate la documentazione e se siete bloccati, aprite un ticket. */
 
 const app = new Vue({
-    el: '#app',
-    data:{
-        logo: './assets/img/logo.png',
-        newTask: '',
-        validateTask: '',
-        tasks: [
-            'Impara HTML',
-            'Impara CSS',
-            'Impara JS',
-        ],
-        completedTasks: [],
+  el: "#app",
+  data: {
+    logo: "./assets/img/logo.png",
+    newTask: "",
+    error: "",
+    tasks: ["Impara HTML", "Impara CSS", "Impara JS"],
+    completedTasks: [],
+    trash: [],
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.length > 3) {
+        this.tasks.unshift(this.newTask);
+        this.newTask = "";
+        this.error = "";
+      } else {
+        this.error = "Inserisci almeno 3 caratteri";
+      }
     },
-    methods: {
-        addTask(){
-            /* if (this.newTask == '' || this.newTask == ' ' || this.newTask == '  ') {
-            this.validateTask = true;
-            } */
-            this.tasks.push(this.newTask);
-            this.newTask = '';
-        },
-        removeTask(index){
-            console.log('Remove task n.' + index);
-            this.tasks.splice(index, 1);
-        },
-        taskCompleted(index, task){
-            this.completedTasks.unshift(task);
-            this.tasks.splice(index, 1);    
-        },
-        updateTask(){
-            alert('Succesfully updated!')
-        },
-        restoreTask(index, task){
-            this.tasks.unshift(task);
-            this.completedTasks.splice(index, 1);
-        }
+    removeTask(index, task) {
+      console.log("Remove task n." + index);
+      this.tasks.splice(index, 1);
+      this.trash.unshift(task);
     },
-    mounted(){
-        window.addEventListener('keyup', (e) =>{
-            if (e.key === 'Enter'){
-                this.addTask();
-            }
-        });
-    }
+    taskCompleted(index, task) {
+      this.completedTasks.unshift(task);
+      this.tasks.splice(index, 1);
+    },
+    updateTask() {
+      alert("Succesfully updated!");
+    },
+    restoreTask(index, task) {
+      this.tasks.unshift(task);
+      this.completedTasks.splice(index, 1);
+    },
+    emptyTrash() {
+      const destroy = confirm("Are you sure you want to empty the bin?");
+      if (destroy) {
+        this.trash = [];
+      }
+    },
+    restoreTrashedTasks(index, task) {
+      this.tasks.unshift(task);
+      this.trash.splice(index, 1);
+    },
+  },
+  mounted() {
+    window.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") {
+        this.addTask();
+      }
+    });
+  },
 });
